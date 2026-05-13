0876daf80d95c16794990ec0bd132330fd7fc135
Silas <ortegasilas5@gmail.com>
2026-05-13 19:47:22 -0300

    chore: Estrutura inicial do projeto

---
+++ ProjetoWeb1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoWeb1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
